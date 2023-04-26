9afba544085039701b1e8b27b35fce376496795c
Theo Okagawa <okagawatheo@gmail.com>
2023-04-26 20:12:48 -0300

    ex_10 adicionado

--- lista_exercicios_01/ex_10.c
+++ lista_exercicios_01/ex_10.c
@@ -2,5 +2,55 @@
 
 int main(){
 
-    //fazer com tuplas e append
+    int num;
+
+    while(num =! 0){
+
+        printf("\nDigite um DDD para consultar na lista:  \n");
+        scanf("%d",&num);
+        
+        
+        switch (num){
+        case 61:
+        printf("Brasilia\n");
+        break;
+
+        case 71:
+        printf("Salvador\n");
+        break;
+
+        case 11:
+        printf("São Paulo\n");
+        break;
+
+        case 21:
+        printf("Rio de Janeiro\n");
+        break;
+
+        case 32:
+        printf("Juiz de Fora\n");
+        break;
+
+        case 19:
+        printf("Campinas\n");
+        break;
+
+        case 27:
+        printf("Vitória\n");
+        break;
+
+        case 31:
+        printf("Belo Horizonte\n");
+        break;
+
+        case 0:
+        break;
+
+        default:
+        printf("Valor Inválido!\n");
+        break;
+    }
+    }
+    
+
 }
