0de9a6aa398301feabc5ef373ee24be48e30ae94
Theo Okagawa <63313909+OkagawaTheo@users.noreply.github.com>
2023-04-28 15:15:16 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -11,10 +11,10 @@ public class Main {
         int D = s.nextInt();
         
         
-        if ((B >C) && (D>A) && ((C+D) > (A + B)) && (C > 0) && (D > 0) && ((A%2) == 0)){
-            System.out.printf("Valores aceitos");
+        if ((B > C) && (D > A) && ((C + D) > (A + B)) && (C > 0) && (D > 0) && ((A%2) == 0)){
+            System.out.println("Valores aceitos");
         } else {
-            System.out.printf("Valores nao aceitos");
+            System.out.println("Valores nao aceitos");
             
         }
     
