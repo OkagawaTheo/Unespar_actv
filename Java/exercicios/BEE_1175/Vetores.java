3124c7041d53922551d578d88aceeb4279ee68ad
Theo <okagawatheo@gmail.com>
2023-07-25 08:14:43 -0300

    ex de java

---
+++ Java/exercicios/BEE_1175/Vetores.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Vetores {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int vetor[] = new int[20];
+
+        for (int i=0;i<vetor.length;i++){
+            vetor[i] = s.nextInt();
+        }
+        for(int i=0;vetor.length / 2; i++){
+            
+        }
+    }
+}
