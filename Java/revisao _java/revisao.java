5655fa1f9676ecfaa9526772556e92f096c0ceff
Theo <okagawatheo@gmail.com>
2023-07-28 14:19:46 -0300

    revisao strigs de java

--- Java/revisao _java/revisao.java
+++ Java/revisao _java/revisao.java
@@ -1,35 +1,55 @@
-import java.util.Scanner;
-import java.util.Random;
+// // import java.util.Scanner;
+// // import java.util.Random;
 
-public class revisao{
-    public static void main(String args[]){
-        Scanner s = new Scanner(System.in);
+// // public class revisao{
+// //     public static void main(String args[]){
+// //         Scanner s = new Scanner(System.in);
  
 
-        // int divisao = 5/2 // output de 2 por ser int
-        // double div = 5.0/2.0 // output de 2,5 
+// //         // int divisao = 5/2 // output de 2 por ser int
+// //         // double div = 5.0/2.0 // output de 2,5 
 
-        // operadores logicos:
-        // and &&
-        // or ||
-        // nor !
+// //         // operadores logicos:
+// //         // and &&
+// //         // or ||
+// //         // nor !
  
 
-        // while loop
-        // int num = 0;
-        // while (num < 10){
-            // num++;
-            // System.out.println(num);
+// //         // while loop
+// //         // int num = 0;
+// //         // while (num < 10){
+// //             // num++;
+// //             // System.out.println(num);
   
-    // }
+// //     // }
 
-        // for loop
-// 
-        // for(int i=0;i<10;i++){
-            // System.out.println(i);
-        // }
+// //         // for loop
+// // // 
+// //         // for(int i=0;i<10;i++){
+// //             // System.out.println(i);
+// //         // }
     
-    
+// //     }}
 
-    
-    }}
+// public class revisao { // Strings
+//     public static void main(String args[]){
+//         String str = "Hello World";
+//         System.out.println(str.charAt(6));
+//         System.out.println("abc".compareTo("aab"));
+//         System.out.println(str.contains("Hello"));
+//         System.out.println(str.hashCode());
+//         System.out.println(str.indexOf("llo",5));
+//         System.out.println("".isBlank());
+//         System.out.println(str.repeat(2));
+//         System.out.println(str.replace(" ", "aaaaaa"));
+//         String arr[] = str.split(" ");
+//         System.out.println(arr[0]);
+//         System.out.println(arr[1]);
+
+//         char arr_chars[] = str.toCharArray();
+//         for (int i=0;i < arr_chars.length; i++){
+//             System.out.println(arr_chars[i]);
+//         }
+//         System.out.println(str.toUpperCase());
+//     }
+// }
