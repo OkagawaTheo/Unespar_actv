688c448a6a55423b43b55efc877745cfbb5773f9
Theo <okagawatheo@gmail.com>
2023-07-30 12:01:31 -0300

    ex a12.c adicionado

--- C/Nivelamento_2/a12.c
+++ C/Nivelamento_2/a12.c
@@ -2,31 +2,30 @@
 
 int main(){
 
-    float peso,altura,m_idade;
-    float idade,y,x,z;
+    float peso,m_idade;
+    float x,count;
+    int idade, altura;
 
-    for (int i=0;i<2;i++){
-        printf("Digite idade peso(kg) e altura(m) respectivamente: ");
-        scanf("%f %f %f",&idade,&peso,&altura);
+    for (int i=0;i<10;i++){
+        printf("Digite idade peso(kg) e altura(cm) respectivamente: ");
+        scanf("%d %f %d",&idade,&peso,&altura);
+
         m_idade += idade;
         
-
-        if (peso >= 90 && altura <= 1,50){
+        if (peso >= 90 && altura <= 150){
             x += 1;
         };
 
-        if (altura > 1,9){
-            y += 1;
+        if (altura >= 190){
             if (idade >= 10 && idade <= 30){
-                z += 1;
+                count += 1;
             }
+            
         }
-
-        
 
     }
 
 printf("Em média %.2f anos\n",m_idade / 2);
 printf("Existem %.2f pessoas com mais de 90 kg e menores que 1.50m\n",x);
-printf("%.2f porcento de pessoas com idade entre 10 e 30 anos entre pessoas com mais de 1.90 de altura.\n",(z/y) * 100);
+printf("%.2f porcento de pessoas com idade entre 10 e 30 anos entre pessoas com mais de 1.90 de altura.\n",(count/10)*100);
 }
