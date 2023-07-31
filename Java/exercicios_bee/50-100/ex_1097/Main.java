80d2c997809a4a8e6c46fba011d34a55a2a4d614
Theo <okagawatheo@gmail.com>
2023-07-31 17:29:18 -0300

    ex java

--- Java/exercicios_bee/50-100/ex_1097/Main.java
+++ Java/exercicios_bee/50-100/ex_1097/Main.java
@@ -1,10 +1,11 @@
 public class Main{
     public static void main(String args[]) {
-
+        int j = 7;
         for (int i=1;i<=9;i+=2){
-            for (int k=7;k>=5;k--){
+            for (int k=j;k>=j-2;k--){
                 System.out.printf("I=%d J=%d\n",i,k);
             }
+            j += 2;
         }
     }
 }
