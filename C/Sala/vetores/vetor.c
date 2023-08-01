6ac61f8b9d0fe4baf315e101daee9a10a26ec5af
Theo <okagawatheo@gmail.com>
2023-08-01 11:16:24 -0300

    ex 1114 java adicionado

--- C/Sala/vetores/vetor.c
+++ C/Sala/vetores/vetor.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main(){
+
+    
     
 }
+
+
+
+// char palava[];
+// oi = "o = palavra[0]"
+//  "i = palavra[1]"
+
+// int num[5] = {1,2,3,4,5};
+// int valores[5] = {2,4,6} == valores[5] = {2,4,6,0,0}
+
+
