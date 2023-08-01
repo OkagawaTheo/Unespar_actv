de2a930e5dbfc9190783b41546bc37d7fca6b20f
Theo <okagawatheo@gmail.com>
2023-08-01 11:24:03 -0300

    arquivo de revisao de vetores em c

--- C/Sala/vetores/vetor.c
+++ C/Sala/vetores/vetor.c
@@ -15,4 +15,9 @@ int main(){
 // int num[5] = {1,2,3,4,5};
 // int valores[5] = {2,4,6} == valores[5] = {2,4,6,0,0}
 
+// atribuição de valores em vetores
+// valor[5];
+// for(int i=0;i<5;i++){
+    // scanf("%d",&valor[i]);
+// }
 
