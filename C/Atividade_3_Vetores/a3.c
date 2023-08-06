e2a1e7a7eaf846d3a042bdc25947da14c3f222a7
Theo <okagawatheo@gmail.com>
2023-08-06 17:21:07 -0300

    ex a3 e a4 adicionados

--- C/Atividade_3_Vetores/a3.c
+++ C/Atividade_3_Vetores/a3.c
@@ -3,11 +3,31 @@
 #include <time.h>
 
     int main(){
+    int qtd1 = 0, qtd2 = 0, qtd3 = 0, qtd4 = 0, qtd5 = 0, qtd6 = 0;   
+
+    int valores[20];
     
+
+    srand(time(NULL));
+
     for (int i=0;i<20;i++){
-        int r = rand() % 6 + 1;
-        printf("%d %d\n",r,i);    
-            
-    }
+        valores[i] = rand() % 6 + 1;        
+        printf("%d\n",valores[i]);
+       
+        if (valores[i] == 1)qtd1++; 
+        else if (valores[i] == 2)qtd2++;
+        else if(valores[i]== 3)qtd3++;
+        else if(valores[i] == 4)qtd4++;
+        else if(valores[i] == 5)qtd5++;
+        else if(valores[i] == 6)qtd6++;
 
     }
+    printf("Quantidade de dados com valor 1 = %d\n", qtd1);
+    printf("Quantidade de dados com valor 2 = %d\n", qtd2);
+    printf("Quantidade de dados com valor 3 = %d\n", qtd3);
+    printf("Quantidade de dados com valor 4 = %d\n", qtd4);
+    printf("Quantidade de dados com valor 5 = %d\n", qtd5);
+    printf("Quantidade de dados com valor 6 = %d\n", qtd6);
+    
+    }
+    
--- C/Atividade_3_Vetores/a4.c
+++ C/Atividade_3_Vetores/a4.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
+    int v[6];
+    int pares[6];
+    int imp[6];
+
+    int par=0,impar = 0;
+    srand(time(NULL));
+
+    for (int i=0;i<6;i++){
+        v[i] = rand() % 5+1;
+
+        if (v[i] % 2 == 0){ // pegar pares
+            pares[i] = v[i];
+            par++;
+        } 
+        else{
+            imp[i] = v[i];
+            impar++;
+        }
+        
+    }
+
+
     
+    printf("Quantidade de valores pares: %d\n",par);
+    printf("Todos os números pares: ");
+    for (int i=0;i<6;i++){
+        if (pares[i] != 0){
+            printf("%d ",pares[i]);
+        }
+
+    }
+    
+    printf("\nQuantidade de valores ímpares: %d\n",impar);
+    printf("Todos os números ímpares: ");
+    for (int i=0;i<6;i++){
+        if (imp[i] != 0){
+            printf("%d ",imp[i]);
+        }
+
+    }
 }
