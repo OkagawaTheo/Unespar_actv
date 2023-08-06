8139daab1089042a435bd4405085ecf63e019f9f
Theo <okagawatheo@gmail.com>
2023-08-06 18:55:48 -0300

    ex_1115 java

--- Java/exercicios_bee/50-100/ex_1115/Main.java
+++ Java/exercicios_bee/50-100/ex_1115/Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String args[]){
-        
+        Scanner s = new Scanner(System.in);
 
+        int x,y;
+
+        while (((x = s.nextInt())!= 0) && ((y=s.nextInt())!= 0)){
+            if (x > 0 && y > 0){
+                System.out.println("primeiro");
+            }
+            else if (x > 0 && y < 0){
+                System.out.println("quarto");
+            }
+            else if (x < 0 && y < 0){
+                System.out.println("terceiro");
+            }
+            else {
+                System.out.println("segundo");
+            }
+        }
     }
 }
 
