46d75e5c2fca6989d108f8d5d912f2517663e075
Theo <okagawatheo@gmail.com>
2023-08-08 14:27:43 -0300

    ultimos ex java finalmente

---
+++ Java/exercicios_bee/50-100/ex_1180/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] x = new int[n];
+        int menor = 0, posMenor = 0;
+        for (int i = 0; i < n; i++) {
+        	x[i] = s.nextInt();
+        	if (i == 0) {
+        		menor = x[i];
+        		posMenor = i;
+        	} else if (x[i] < menor) {
+        		menor = x[i];
+        		posMenor = i;
+        	}
+        }
+        System.out.println("Menor valor: " + menor);
+        System.out.println("Posicao: " + posMenor);
+    }
+	
+}
