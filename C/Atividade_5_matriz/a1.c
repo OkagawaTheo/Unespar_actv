e889cd282e517e61b3ac8f7a08d4e643dd166053
Theo Okagawa <okagawatheo@gmail.com>
2023-08-18 16:45:18 -0300

    ex a1.c

---
+++ C/Atividade_5_matriz/a1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int mat[3][3];
+    int maior=0;
+
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            scanf("%d",&mat[i][j]); //scanf com espaço pede mais um numero
+            if (mat[i][j] > maior) maior=mat[i][j];
+        }
+    }
+
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            printf("%d ",mat[i][j]);
+        }
+    }
+    printf("\nO maior número digitado foi: %d",maior);
+}
