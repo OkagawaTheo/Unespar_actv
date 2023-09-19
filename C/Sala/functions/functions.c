6502f3eadb854d0c91d0615e1fd021a8b9639bf1
Theo Okagawa <okagawatheo@gmail.com>
2023-09-19 11:33:02 -0300

    functions att

---
+++ C/Sala/functions/functions.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+void maiorNumero(double a, double b){
+    if (a > b){
+        printf("Número %lf é maior.",a);
+    } else if (a < b){
+        printf("Número %lf é maior.",b);
+    } else printf("Iguais.");
+}
+
+
+void squareRoot(double num){
+    printf("%lf",sqrt(num));
+}
+
+void fat(int num){
+    int fat;
+    for(fat = 1; num > 1; num = num - 1){
+        fat *= num;
+    }
+    printf("%d",fat);
+}
+
+int main(){
+    double a,b,num;
+    printf("Digite os dois números: ");
+    scanf("%lf %lf",&a,&b);
+    maiorNumero(a,b);
+
+    printf("Digite um número para saber sua raiz e fatorial: ");
+    scanf("%lf",&num);
+
+    squareRoot(num);
+    fat(num);
+}
