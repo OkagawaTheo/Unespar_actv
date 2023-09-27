5687cc5d90a3ff0e1ae51b7594d921ca086c787f
Theo Okagawa <okagawatheo@gmail.com>
2023-09-27 17:46:21 -0300

    echo -e adicionado

--- x.sh
+++ x.sh
@@ -9,9 +9,14 @@
 #sudo apt-get install --install-recommends winehq-stable
 #sudo apt-get install --install-recommends winehq-devel
 
+WINEARCH=win32 WINEPREFIX=~/.wine-circuitmaker winetricks -q gdiplus corefonts riched20 mdac28 msxml6 dotnet40
+curl http://web.archive.org/web/20160129053851/http://download.microsoft.com/download/E/6/A/E6A04295-D2A8-40D0-A0C5-241BFECD095E/W2KSP4_EN.EXE -o ~/.cache/winetricks/win2ksp4/W2KSP4_EN.EXE
+WINEARCH=win32 WINEPREFIX=~/.wine-circuitmaker winetricks -q gdiplus corefonts riched20 mdac28 msxml6 dotnet40
 
+curl https://docentes.ifrn.edu.br/leonardoteixeira/links/instalador-do-circuitmaker-student/at_download/file -0 ~/Downloads/circuitmaker_student.exe
+
+WINEPREFIX=~/.wine-circuitmaker wine ~/Downloads/circuitmaker_student.exe
+echo -e "\n\nInstalado com sucesso! Desce o follow ai pra ajudar"
 circuitmaker
- echo -e "\n\nInstalado com sucesso! Desce o follow ai pra ajudar"
-
 
 # caso dê errado, entre nesse link <https://docente.ifrn.edu.br/leonardoteixeira/links/instalador-do-circuitmaker-student/view>, baixe manualmente e execute
