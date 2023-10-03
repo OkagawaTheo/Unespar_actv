4dea9dead048aee73f3f98fcb64431ecb937e29e
Theo Okagawa <okagawatheo@gmail.com>
2023-10-03 15:20:11 -0300

    a2 adicionado

---
+++ C/Atividade_7_functions/a2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+typedef struct Funcionario{
+    char nome[30];
+    int idade;
+    char dataNasc[15];
+    char sexo;
+    char cpf[20];
+    int codSetor;
+    char cargo[30];
+    float salario;
+}func;
+
+
+void catchInfo(func *f){
+    printf("Digite o nome:");
+    printf("Digite a idade:");
+    printf("Digite a data de nascimento:");
+    printf("Digite o sexo:");
+    printf("Digite o cpf:");
+    printf("Digite o código do setor:");
+    printf("Digite o cargo:");
+    printf("Digite o salário:");
+
+
+    
+}
+
+
+
+
+int main(){
+
+}
