320159b614133689e61ea4b845285f490f92e4be
Theo Okagawa <okagawatheo@gmail.com>
2023-10-24 09:17:08 -0300

    erros de buffer resolvidos

--- C/Atividade_9_Files/a3.c
+++ C/Atividade_9_Files/a3.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
-typedef struct Aluno{
+typedef struct Aluno {
     int numero;
     char nome[50];
     char curso[30];
     float nota1, nota2;
-}al;
+} Aluno;
 
-
-
-int main(){
-    al aluno;
+int main() {
+    Aluno aluno;
     char nomeArq[100];
 
     printf("Número do aluno: ");
-    scanf("%d",&aluno.numero);
-    getchar();
+    scanf("%d", &aluno.numero);
+    getchar(); // Consumir a quebra de linha deixada no buffer
+
     printf("Digite o nome do aluno: ");
-    fgets(aluno.nome,sizeof(aluno.nome),stdin);
-
-    printf("Curso: ");
-    fgets(aluno.curso,sizeof(aluno.curso),stdin);
-
-    printf("1º nota: ");
-    scanf("%f",&aluno.nota1);
-
-    printf("2º nota: ");
-    scanf("%f",&aluno.nota2);
+    fgets(aluno.nome, sizeof(aluno.nome), stdin);
 
     snprintf(nomeArq, sizeof(nomeArq), "%s.txt", aluno.nome);
+    FILE *arq = fopen(nomeArq, "w");
 
-    fprintf(arq, "Número: %d\n",aluno.numero);
-    fprintf(arq, "Nome: %d\n",aluno.nome);
-    fprintf(arq, "Curso: %d\n",aluno.curso);
-    fprintf(arq, "Nota 1: %d\n",aluno.nota1);
-    fprintf(arq, "Nota 2: %d\n",aluno.nota2);
-    
+    printf("Curso: ");
+    fgets(aluno.curso, sizeof(aluno.curso), stdin);
+
+    printf("1º nota: ");
+    scanf("%f", &aluno.nota1);
+
+    printf("2º nota: ");
+    scanf("%f", &aluno.nota2);
+
+    fprintf(arq, "Número: %d\n", aluno.numero);
+    fprintf(arq, "Nome: %s\n", aluno.nome);
+    fprintf(arq, "Curso: %s\n", aluno.curso);
+    fprintf(arq, "Nota 1: %.2f\n", aluno.nota1);
+    fprintf(arq, "Nota 2: %.2f\n", aluno.nota2);
+
     fclose(arq);
-    printf("Informações salvas no arquivo %s.\n",nomeArq);
+    printf("Informações salvas no arquivo %s.\n", nomeArq);
 
-
+    return 0;
 }
