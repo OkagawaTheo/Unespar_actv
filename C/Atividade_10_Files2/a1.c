5d8c2344e6abc2561e78326b0dd3eda53dc01ed4
Theo Okagawa <okagawatheo@gmail.com>
2023-10-27 10:40:13 -0300

    a1 em andamento

---
+++ C/Atividade_10_Files2/a1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct Texto{
+    char nome[100];
+
+
+}Texto;
+
+
+int main()
+{
+    char nomeArq[100];
+    FILE* arq;
+    Texto txt;
+
+    printf("\nDigite o nome do seu arquivo: ");
+    fgets(txt.nome, sizeof(txt.nome),stdin);
+    snprintf(nomeArq,sizeof(nomeArq),"%s.txt",txt.nome);
+    printf("%s",nomeArq);
+    
+    if (( arq = fopen(nomeArq, "w")) == NULL){
+        printf("\nNao consigo abrir o arquivo ! ");
+    }
+    
+    printf("O que deseja inserir dentro do arquivo? ");
+    
+}
