6a9717f56c5fc81c709dc58b93b18863df792d0b
Theo Okagawa <okagawatheo@gmail.com>
2023-11-05 21:35:38 -0300

    a6.c

---
+++ C/Atividade_10_Files2/a6.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+typedef struct Lojas{
+    char num[5];
+    float total_vendas;
+}filial;
+
+int main(){
+    FILE* arq;
+    filial f;
+    int qtd_cadastros;
+    int filial;
+    float valor_total;
+    
+    printf("Quantas vendas deseja cadastrar? ");
+    scanf("%d",&qtd_cadastros);
+    if ((arq = fopen("entrada.txt","w")) == NULL){
+        printf("Erro ao abrir");
+    }
+    
+
+    for (int i=0;i<qtd_cadastros;i++){
+        printf("Cadastrando a %dº venda",i+1);
+        setbuf(stdin, NULL);
+        printf("\nCódigo da filial: ");
+        scanf("%s",&f.num);
+        
+        printf("Digite o total de vendas: ");
+        scanf("%f",&f.total_vendas);
+        
+        fprintf(arq,"%s,%.2f\n",f.num,f.total_vendas);
+    }
+    fclose(arq);
+    
+    arq = fopen("entrada.txt","r");
+    
+}
