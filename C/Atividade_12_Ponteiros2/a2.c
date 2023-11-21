9557df2103a251a65fb95e760d1d83211f57b04a
Theo Okagawa <okagawatheo@gmail.com>
2023-11-21 15:18:30 -0300

    feat: função somar dobro

---
+++ C/Atividade_12_Ponteiros2/a2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void SomaDobro(int *a,int *b){
+
+
+}
+
+int main(){
+    int a,b;
+
+    printf("Digite dois valores inteiros: ");
+    scanf("%d %d",&a,&b);
+    SomaDobro(&a,&b);
+}
