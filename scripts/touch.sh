f7ee5198365f2948a5dcf0064c171b2ccb6c1481
Theo Okagawa <okagawatheo@gmail.com>
2023-11-27 10:19:34 -0300

    path alterado

--- scripts/touch.sh
+++ scripts/touch.sh
@@ -4,4 +4,3 @@ get_id=$(xinput list | grep "ETPS/2 Elantech Touchpad" | cut -d '=' -f '2' | cut
 xinput set-button-map $get_id 1 0 3
 xinput set-prop $get_id "libinput Scrolling Pixel Distance" 50
 xinput set-prop $get_id "libinput Accel Speed" 0.55
-
