8ba8496541142b67bfd44dee3791f2a7a8f1b46a
Theo Okagawa <okagawatheo@gmail.com>
2023-12-11 21:52:35 -0300

    a4.c

---
+++ C/Atividade_13_malloc/a4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int linhas, colunas;
+
+    printf("Digite o número de linhas: ");
+    scanf("%d", &linhas);
+
+    printf("Digite o número de colunas: ");
+    scanf("%d", &colunas);
+
+    int **matriz = (int**)malloc(linhas * sizeof(int*));
+
+    for (int i = 0; i < linhas; i++) {
+        matriz[i] = (int*)malloc(colunas * sizeof(int));
+    }
+
+    printf("Digite os elementos da matriz:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("Elemento (%d, %d): ", i + 1, j + 1);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    printf("Matriz inserida:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
