6adcf0d3f1b44319ea793aa304374193db48c16f
Theo Okagawa <okagawatheo@gmail.com>
2024-04-04 10:17:24 -0300

    Pastas agora são separadas por ano de graduação.
    Pastas para materias do segundo ano também criadas

--- touch.sh
+++
@@ -1,7 +0,0 @@
-
-get_id=$(xinput list | grep "ETPS/2 Elantech Touchpad" | cut -d '=' -f '2' | cut -f 1)
-
-xinput set-button-map $get_id 1 0 3
-xinput set-prop $get_id "libinput Scrolling Pixel Distance" 50
-xinput set-prop $get_id "libinput Accel Speed" 0.55
-
