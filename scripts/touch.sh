8a80832c5393f9f990157456da07b7a43ca0aa75
Theo <okagawatheo@gmail.com>
2024-04-09 23:18:02 -0300

    linha adicionada para ativar o natural scrolling (touchpad scrolla em sentido inverso

--- scripts/touch.sh
+++ scripts/touch.sh
@@ -2,5 +2,6 @@
 get_id=$(xinput list | grep "ETPS/2 Elantech Touchpad" | cut -d '=' -f '2' | cut -f 1)
 
 xinput set-button-map $get_id 1 0 3
-xinput set-prop $get_id "libinput Scrolling Pixel Distance" 50
-xinput set-prop $get_id "libinput Accel Speed" 0.55
+#xinput set-prop $get_id "libinput Scrolling Pixel Distance" 50
+#xinput set-prop $get_id "libinput Accel Speed" 0.55
+xinput set-prop $get_id "libinput Natural Scrolling Enabled Default" 1
