e1815599fccb5f396704152b30f92638499f2747
Theo <okagawatheo@gmail.com>
2024-04-18 11:16:31 -0300

    ex kleber

---
+++ 2_ano/estrutura_dados/ex001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// Aluno: Theo Okagawa
+
+main(){ //incremento
+    int c; 
+    c = 5;
+    printf("%d \n", c);
+    printf("%d \n", c++);
+    printf("%d \n\n", c);
+    c = 5;
+    printf("%d \n", c);
+    printf("%d \n", ++c);
+    printf("%d \n\n", c);
+
+}
--- 2_ano/estrutura_dados/ex002.c
+++ 2_ano/estrutura_dados/ex002.c
@@ -1,21 +1,6 @@
 
 #include <stdio.h>
-// Aluno: Theo Okagawa
-
-main(){ //incremento
-    int c; 
-    c = 5;
-    printf("%d \n", c);
-    printf("%d \n", c++);
-    printf("%d \n\n", c);
-    c = 5;
-    printf("%d \n", c);
-    printf("%d \n", ++c);
-    printf("%d \n\n", c);
-
-}
-#include <stdio.h>
-main(){// aritmetica
+int main(){// aritmetica
     
     i+= 2;       //i=(i+2)
     x*=y+1;     //x=x*(y+1)
@@ -25,39 +10,7 @@ main(){// aritmetica
 
 }
 
-#include <stdio.h> //idade
-int main(){
-    int idade;
-    printf("Digite a idade: "); scanf("%d",&idade);
-    if (idade > 25){
-        printf("maduro");
-    } else if(idade > 60){
-        printf("Idoso");
-    } else if (idade >= 17 && idade <= 25){
-        printf("jovem adulto");
-    }else if (idade < 18){
-        printf("jovem");
-    }
-}
 
-main(){
-    int numero;
-    printf("Digite um numero");
-    scanf("%d",&numero);
 
-    if (num == 10){
-        printf("vc acertou");
-    } else{
-        if (num > 10){
-            printf("numero maior q 10");
-        }else{
-            printf("numero menor que 10");
-        }
-    }
-}
 
-main(){
-    for(int i=0;i<10;i++){
-        printf("%d\n",i);
-    }
-}
+
