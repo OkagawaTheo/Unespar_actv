216416ae62d6b318415b0a2c981e43b98448482c
Theo <okagawatheo@gmail.com>
2024-04-25 10:53:21 -0300

    quad

--- 2ano/estrutura_dados/TDA/calc.c
+++ 2ano/estrutura_dados/TDA/calc.c
@@ -1,34 +1,9 @@
-#include "calc.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.c"
+#include "retan.h"
 
-Calculadora* criarCalculadora() {// inicia a calculadora
-
-    Calculadora *calc = (Calculadora*)malloc(sizeof(Calculadora));
-    if (calc != NULL) {
-        calc->resultado = 0;
-    }
-    return calc;
-}
-
-void destruirCalculadora(Calculadora *calc) {
-    free(calc);
-}
-
-void somar(Calculadora* calc, int valor) {
-    calc->resultado += valor;
-}
-
-void subtrair(Calculadora* calc, int valor) {
-    calc->resultado -= valor;
-}
-
-void multiplicar(Calculadora* calc, int valor) {
-    calc->resultado *= valor;
-}
-
-void imprimirResultado(const Calculadora* calc) {
-    printf("Resultado: %d\n", calc->resultado);
-}
-
+int main(){
+    Retangulo meuRetangulo = criarRetangulo(5.0,3.0);
+    
+    float area = calcularArea(meuRetangulo);
+    printf("A area do retangulo é %.2f\n",area);
+}z
