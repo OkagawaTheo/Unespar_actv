47529a2d9861674d40f822e846ae6f40a8715d28
Theo <okagawatheo@gmail.com>
2024-05-15 20:04:26 -0300

    ex01

---
+++ 2ano/java2ano/aula_06/ex01/BombaAgua.java
@@ -0,0 +1,23 @@
+public class BombaAgua {
+
+    boolean status;
+
+    void ligar(){
+        this.status = true;
+        System.out.println("Bomba Ligada");
+
+    }
+    void desligar(){
+        this.status = false;
+        System.out.println("Bomba Desligada.");
+    }
+
+    
+    void intervaloBomba(){
+        for(int i=10;i>=0;i--){
+            System.out.printf("Bomba ligada.Tempo restante: %d\n",i);
+        }
+        
+    }
+
+}
---
+++ 2ano/java2ano/aula_06/ex01/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String args[]){
+        BombaAgua bomba = new BombaAgua();
+        
+        bomba.ligar();
+        bomba.intervaloBomba();
+        bomba.desligar();
+    }
+
+}
