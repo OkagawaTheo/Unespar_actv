bc304f149346c05db6b3ff9d76b3691a2b2bf982
Theo <okagawatheo@gmail.com>
2024-06-18 14:23:19 -0300

    função for para estados finais

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ class Turing:
         self.estados_finais: list[str] = []
         self.marcador_inicio: str = ""
         self.marcador_final: str = ""
+        # __get__ __set__
         
     def catchInfo(self) -> None:
-        self.qtd_letras = int(input("Digite a quantidade de letras do alfabeto: "))
+        self.qtd_letras = int(input("\nDigite a quantidade de letras do alfabeto: "))
 
         for i in range(self.qtd_letras):
             letra = input(f"\tDigite a letra {i+1}: ")
@@ -20,9 +21,17 @@ class Turing:
         self.qtd_alfabeto_aux = int(input("Digite a quantidade de letras do alfabeto auxiliar: "))
 
         for i in range(self.qtd_alfabeto_aux):
-            letra = input("Digite a letra do alfabeto auxiliar: ")
+            letra = input(f"\tDigite a letra {i+1} do alfabeto auxiliar: ")
             self.alfabeto_aux.append(letra)
-            print(self.alfabeto_aux[0])
-            print(self.alfabeto[0])    
+
+        self.qtd_estados = int(input("Digite a quantidade de estados: "))
+        self.estado_inicial = int(input("Informe o estado inicial: "))
+        self.qtd_estados_finais = int(input("Digite a quantidade de estados finais: "))
+        for i in range(self.qtd_estados_finais):
+            letra = input(f"\tDigite o estado final {i+1}: ")
+
+
+    
+
 maquina1 = Turing()
 maquina1.catchInfo()
