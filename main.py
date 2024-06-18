10cf8592349c804c746d09101e420227e71e50a8
Theo <okagawatheo@gmail.com>
2024-06-18 08:46:28 -0300

    for func para pegar o alfabeto auxiliar

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ class Turing:
     def __init__(self):
         self.qtd_letras: int = 0
         self.alfabeto: list[str] = []
+        self.qtd_alfabeto_aux: int = 0
         self.alfabeto_aux: list[str] = []
         self.qtd_estados: int = 0
         self.estado_inicial: str = ""
@@ -14,8 +15,14 @@ class Turing:
         self.qtd_letras = int(input("Digite a quantidade de letras do alfabeto: "))
 
         for i in range(self.qtd_letras):
-            letra = input(f"Digite a letra {i+1}: ")
+            letra = input(f"\tDigite a letra {i+1}: ")
             self.alfabeto.append(letra)
-        
+        self.qtd_alfabeto_aux = int(input("Digite a quantidade de letras do alfabeto auxiliar: "))
+
+        for i in range(self.qtd_alfabeto_aux):
+            letra = input("Digite a letra do alfabeto auxiliar: ")
+            self.alfabeto_aux.append(letra)
+            print(self.alfabeto_aux[0])
+            print(self.alfabeto[0])    
 maquina1 = Turing()
 maquina1.catchInfo()
