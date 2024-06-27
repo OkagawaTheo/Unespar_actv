bbb33c385bfe8530fa83c53dfeb4ac62255fd1ab
Theo <okagawatheo@gmail.com>
2024-06-27 08:50:14 -0300

    codigo do cilindro copiado so falta arrumar a logica

--- 2ano/c2ano/AOC/escalonamento.c
+++ 2ano/c2ano/AOC/escalonamento.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void fitaCilindro(int tamanho_cilindro, char transicoes[]) {
-    printf("\nRepresentação do cilindro FCFS:\n");
+    printf("\nRepresentação do cilindro SCAN:\n");
     
     for (int i = 0; i < tamanho_cilindro; i++) {
         printf("| %2d ", i + 1);  
@@ -15,11 +16,16 @@ void fitaCilindro(int tamanho_cilindro, char transicoes[]) {
 }
 
 void marcarCilindroEscalonamento(int tamanho_cilindro, int qtd_pedidos, char transicoes[], int pedidos_cilindro[]) {
-    for (int i = 0; i < qtd_pedidos; i++) {
-        transicoes[pedidos_cilindro[i] - 1] = 'x';
-        fitaCilindro(tamanho_cilindro, transicoes);
+    int menorDistancia = pedidos_cilindro[0];
+    if 
+
+
+    // for (int i=0;i<qtd_pedidos;i++){
+    //     transicoes[menorDistancia-1] = 'x';
+    fitaCilindro(tamanho_cilindro, transicoes);
     }
 }
+
 
 int main() {
     printf("Informe o tamanho do cilindro: ");
@@ -35,7 +41,7 @@ int main() {
     int qtd_pedidos;
     scanf("%d", &qtd_pedidos);
     
-    int pedidos_cilindro[qtd_pedidos];
+    int pedidos_cilindro[qtd_pedidos]; // Cria lista dos pedidos do tamanho da qtd pedida
     for (int i = 0; i < qtd_pedidos; i++) {
         printf("\tInforme o %dº pedido de cilindro: ", i + 1);
         scanf("%d", &pedidos_cilindro[i]);
