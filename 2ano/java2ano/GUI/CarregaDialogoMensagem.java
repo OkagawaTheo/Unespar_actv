743b78a54c5892a161dc0f9e6220f4be40235b02
Theo <okagawatheo@gmail.com>
2024-06-27 08:53:03 -0300

    gui classes

--- 2ano/java2ano/GUI/CarregaDialogoMensagem.java
+++ 2ano/java2ano/GUI/CarregaDialogoMensagem.java
@@ -4,7 +4,7 @@ public class CarregaDialogoMensagem {
 
     public static void main(String[] args){
 
-        JFrame frame = new JFrame ("Uso de texto");
+        JFrame frame = new JFrame ("Uso de diálogo de Informação");
         frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add (new GuiDialogoMensagem());
         frame.setBounds(0,0,500,300);
