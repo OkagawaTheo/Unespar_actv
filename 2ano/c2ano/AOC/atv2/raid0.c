1c668905ccfabd8edd487f2b0c811504e4b8932a
Theo <okagawatheo@gmail.com>
2024-07-02 15:22:54 -0300

    transformei discos em struct para reutilizar em outras raids

--- 2ano/c2ano/AOC/atv2/raid0.c
+++ 2ano/c2ano/AOC/atv2/raid0.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    char frase[100];
+typedef struct discos{
     char disco1[100];
     char disco2[100];
+}disc;
+
+
+int main(){
+    disc discos_raid0;
+    char frase[100];
 
     printf("Simulador de RAID 0\n");
     printf("Digite a frase: ");
     fgets(frase,sizeof(frase),stdin);
     
   for (int i = 0; i < strlen(frase); i++) {
-        if (i % 2 == 0) disco1[i / 2] = frase[i];
-        else disco2[i / 2] = frase[i]; 
+        if (i % 2 == 0) discos_raid0.disco1[i / 2] = frase[i];
+        else discos_raid0.disco2[i / 2] = frase[i]; 
     }
     
-    printf("Disco 1: %s\n", disco1);
-    printf("Disco 2: %s\n", disco2);
+    printf("Disco 1: %s\n", discos_raid0.disco1);
+    printf("Disco 2: %s\n", discos_raid0.disco2);
     
 
 }
