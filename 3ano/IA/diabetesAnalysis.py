a5ade7aea9fca36dbf053f02cc0ed44a6ccea699
Theo Okagawa <okwath@ccomp3-apc.localdomain>
2025-07-18 13:46:16 -0300

    inicio tratamento de variáveis impossíveis

--- 3ano/IA/diabetesAnalysis.py
+++ 3ano/IA/diabetesAnalysis.py
@@ -1,24 +1,35 @@
 import pandas as pd
 
 class DiabetesAnalysis:
-  def __init__(self,file_path,data_columns):
+  def __init__(self, file_path):
+    self.header_columns = ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI", "DiabetesPedigreeFunction", "Age", "Outcome"]
     self.file_path = file_path
-    self.num_columns = data_columns
     self.df = None
+    self.LoadData()
 
   def LoadData(self):
     try:
-      self.df = pd.read_csv(file_path)
+      self.df = pd.read_csv(self.file_path, names=self.header_columns)
       print("Carregado.")
       print(self.df.head())
     except FileNotFoundError:
       print("Arquivo não encontrado.")
+      self.df = None
     except Exception as e:
       print(f"Erro ao carregar o dataSet '{e}'")
+      self.df = None
 
+  def handleImpossibleVar(self):
+    if self.df is None:
+      print("DataFrame nao carregado corretamente")
+      return
+    
+    TroubleCauserVars = ["Glucose", "BloodPressure","SkinThickness","Insulin","BMI"]
+    for col in TroubleCauserVars:
+      CountZeroes = (self.df[col] == 0).sum()
+
+      
 
 if __name__ == "__main__":
   file_path = '/home/okwath/unespar/Unespar_actv/3ano/IA/archive/diabetes.csv'
-  data_columns = ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI","DiabetesPedigreeFunction","Age","Outcome"]
-  instance = DiabetesAnalysis(file_path=file_path,data_columns=data_columns)
-  instance.LoadData()
+  instanceDiabetes = DiabetesAnalysis(file_path=file_path)
